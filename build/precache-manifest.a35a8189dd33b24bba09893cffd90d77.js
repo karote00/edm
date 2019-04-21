97d8acd8d61435cbed3c76691251905689ad143c
Asa Tsai <karoteglathe@gmail.com>
2019-04-21 23:48:55 +0800

    Initial build subtree commit

---
+++ build/precache-manifest.a35a8189dd33b24bba09893cffd90d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b2b4fe725f38e1c8feb7",
+    "url": "/static/js/main.27a7ab39.chunk.js"
+  },
+  {
+    "revision": "3ce012f81cc5c3b611b6",
+    "url": "/static/js/2.69a49555.chunk.js"
+  },
+  {
+    "revision": "b2b4fe725f38e1c8feb7",
+    "url": "/static/css/main.1e3873a3.chunk.css"
+  },
+  {
+    "revision": "cf9129f27264d35bf62198ca85b77375",
+    "url": "/index.html"
+  }
+];
