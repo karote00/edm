4f94e308bd513f660defbead4a7e4fc825f2638a
Asa Tsai <karoteglathe@gmail.com>
2019-04-21 23:47:19 +0800

    add UpperCase Valid helper

---
+++ src/helpers/Valid.js
@@ -0,0 +1,12 @@
+/**
+ * @function isUndefined
+ * @param {any} data - Any type of data
+ * @return If the data is undefined
+ */
+const isUndefined = (data) => {
+  return data === undefined;
+};
+
+export {
+  isUndefined
+};
